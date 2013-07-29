0d925530c74932384bc2db353a2d733945fc8794
Andreas Brekken <a@abrkn.com>
2013-07-29 11:31:48 +0200

    Unbreak util\debug.

--- web/util/debug.js
+++ web/util/debug.js
@@ -4,7 +4,7 @@ module.exports = function(name) {
     name = name || ''
 
     return function() {
-        if (!console || !console.log)
+        if (!console || !console.log) {
             return
         }
 
