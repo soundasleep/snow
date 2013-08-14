1ebfea5bca16387e58bd9600da92e9fcd136dbed
Andreas Brekken <a@abrkn.com>
2013-08-14 17:05:53 +0200

    Extract form validation.

--- web/helpers/jquery.js
+++ web/helpers/jquery.js
@@ -1,5 +1,7 @@
 var _ = require('lodash')
 
+require('../vendor/shake')
+
 $.fn.enabled = function(value) {
     if (typeof value != 'undefined') {
         if (value) $(this).removeAttr('disabled')
@@ -59,9 +61,10 @@ $.fn.focusSoon = function() {
 }
 
 $.fn.field = function(name, value) {
-    var $fields = name === null ?
-        $(this).find('input, select, textarea') :
-        $(this).find('[name="' + name + '"]')
+    var $fields = name ?
+        $(this).find('[name="' + name + '"]') :
+        $(this).find('input, select, textarea')
+
 
     if (value !== undefined) {
         $fields.each(function() {
@@ -88,4 +91,35 @@ $.fn.valOrNull = function(val) {
         return $(this).val(val || '')
     }
     return $(this).val().trim() || null
+}
+
+$.fn.validate = function(emptyIsError) {
+    var $this = $(this)
+    , error
+
+    if (this[0].tagName == 'FORM') {
+        var $groups = $this.find('.form-group')
+        _.each($groups, function(x) {
+            error |= !$(x).validate(emptyIsError)
+        })
+        if (error) {
+            $this.find('.has-error .form-control:first').focus()
+            $this.find('button[type="submit"]').shake()
+        }
+    } else {
+        var check = this[0].check || function() {
+            return !val.match(new RegExp($field.attr('data-regex')))
+        }
+        , $field = $this.field()
+        , val = $field.val()
+        , empty = val.length === 0
+        , invalid = empty ? emptyIsError : check()
+        error = empty ? emptyIsError : invalid
+
+        $this
+        .toggleClass('is-empty', empty)
+        .toggleClass('is-invalid', !empty && invalid)
+        .toggleClass('has-error', error)
+    }
+    return !error
 }
