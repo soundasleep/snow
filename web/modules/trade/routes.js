7c8bd2e34ec3dfd1dbe73b5d20339acd528a42a9
Andreas Brekken <a@abrkn.com>
2013-08-19 13:13:40 +0200

    Remember user mode/market/type with cookie. close justcoin/justcoin#89

--- web/modules/trade/routes.js
+++ web/modules/trade/routes.js
@@ -1,7 +1,14 @@
+var format = require('util').format
+
 module.exports = function(router, master, authorize) {
     return router
     .add(/^trade$/, function() {
-        router.go('trade/BTCNOK/instant/buy', true)
+        var mode = $.cookie('tradeMode') || 'instant'
+        , market = $.cookie('tradeMarket') ||
+            (api.user.country == 'NO' ? 'BTCNOK' : 'BTCEUR')
+        , type = $.cookie('tradeType') || 'buy'
+
+        router.go(format('trade/%s/%s/%s', market, mode, type), true)
     })
     .add(/^trade\/orders$/, function() {
         if (!authorize.user()) return
@@ -11,6 +18,11 @@ module.exports = function(router, master, authorize) {
         function(market, mode, type)
     {
         if (!authorize.user(2)) return
+
+        $.cookie('tradeMode', mode, { expires: 10 * 356 * 7 })
+        $.cookie('tradeMarket', market, { expires: 10 * 356 * 7 })
+        $.cookie('tradeType', type, { expires: 10 * 356 * 7 })
+
         mode = mode == 'instant' ? 'market' : 'limit'
         type = type == 'buy' ? 'bid' : 'ask'
         master(require('./market')(market, mode, type), 'trade')
