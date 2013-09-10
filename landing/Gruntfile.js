8b4aa3be3d03014b7d9dece72be8dad2a7f5e176
Andreas Brekken <a@abrkn.com>
2013-09-10 14:55:13 +0200

    Fix grunt.

--- landing/Gruntfile.js
+++ landing/Gruntfile.js
@@ -4,7 +4,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         bower: {
-            install: {}
+            options: {
+                targetDir: './bower_components'
+            }
         },
         uglify: {
             options: {
