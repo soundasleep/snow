b4808dc2df85b934aca088ca7709e7ec989e7c64
Andreas Brekken <a@abrkn.com>
2013-09-10 13:40:37 +0200

    First commit on frontend grunt.

---
+++ landing/Gruntfile.js
@@ -0,0 +1,60 @@
+var config = require('konfu')
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower: {
+            install: {}
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:mm") %> */\n'
+            },
+            main: {
+                src: 'landing.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            },
+            vendor: {
+                src: ['bower_components/jquery/jquery.min.js', 'vendor/highstock.js'],
+                dest: 'build/vendor.min.js'
+            }
+        },
+        stylus: {
+            compile: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:mm") %> */\n',
+                    'include css': true
+                },
+                files: {
+                    'build/styles.min.css': ['index.styl']
+                }
+            }
+        },
+        ejs: {
+            options: {
+                segment: config.segment
+            },
+            'en-US': {
+                src: 'index.en-US.ejs',
+                dest: 'public/index.html'
+            },
+            'nb-NO': {
+                src: 'index.nb-NO.ejs',
+                dest: 'public/index.no.html'
+            }
+        },
+        img: {
+            all: {
+                src: ['assets/*.png', 'assets/*.jpg'],
+                dest: 'public'
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-img')
+    grunt.loadNpmTasks('grunt-bower-task')
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-contrib-stylus')
+    grunt.loadNpmTasks('grunt-ejs')
+    grunt.registerTask('default', ['bower', 'uglify', 'stylus', 'ejs', 'img'])
+}
