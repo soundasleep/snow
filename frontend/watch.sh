2928a4e79a7d85e1320c8b65b2be37a1281d3c4f
Andreas Brekken <a@abrkn.com>
2013-09-11 14:29:38 +0200

    Make frontend.

---
+++ frontend/watch.sh
@@ -0,0 +1,1 @@
+node /c/git/livewatch/bin/livewatch -d "make public" modules:index.js:helpers:util:landing.js:router.js:i18n:assets:routes:i18n:api.js:app.js:router.js:user.js:caches.js:authorize.js:errors.js derp
