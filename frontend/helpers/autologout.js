68d93bef4c6c4ae7e2f622264eae1a156026b418
Andreas Brekken <a@abrkn.com>
2013-09-17 14:34:45 +0200

    Add re-authenticate support (modal)

--- frontend/helpers/autologout.js
+++ frontend/helpers/autologout.js
@@ -1,11 +1,16 @@
 var debug = require('./debug')('snow:autologout')
 
 module.exports = exports = function() {
-    var timer
+    var timer, started
 
     function restartTimer() {
         if (!api.user) return
         timer && clearTimeout(timer)
+        if (started) {
+            debug('restarting autologout timer. had %ss left',
+                Math.round((30 * 60e3 - (new Date() - started)) / 1e3))
+        }
+        started = +new Date()
         timer = setTimeout(function() {
             if (!api.user) return
             debug('logging out from inactivity')
