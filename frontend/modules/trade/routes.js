3ff245154e93a1705b98ba92006e1e356dcf9a89
Andreas Brekken <a@abrkn.com>
2013-09-17 15:13:11 +0200

    Simplify trade routes.

--- frontend/modules/trade/routes.js
+++ frontend/modules/trade/routes.js
@@ -3,33 +3,46 @@ var format = require('util').format
 module.exports = function(router, master, authorize) {
     return router
     .add(/^trade$/, function() {
-        var mode = $.cookie('tradeMode') || 'instant'
-        , market = $.cookie('tradeMarket') ||
-            (api.user.country == 'NO' ? 'BTCNOK' : 'BTCEUR')
-        , type = $.cookie('tradeType') || 'buy'
-
-        router.go(format('trade/%s/%s/%s', market, mode, type), true)
+        var market = $.cookie('tradeMarket') ||
+            (api.user && api.user.country == 'NO' ? 'BTCNOK' : 'BTCEUR')
+        router.go(format('trade/%s', market), true)
     })
-    .add(/^trade\/orders$/, function() {
-        if (!authorize.user()) return
-        master(require('./orders')(), 'trade')
+    .add(/^trade\/([A-Z]{6})$/, function(market) {
+        router.go(format(
+            'trade/%s/%s',
+            market,
+            $.cookie('tradeMode') == 'advanced' ? 'advanced' : 'instant'),
+        true)
+    })
+    .add(/^trade\/([A-Z]{6})\/stats$/,
+        function(market)
+    {
+        master(require('./stats')(market), 'trade')
+    })
+    .add(/^trade\/([A-Z]{6})\/(advanced|instant)$/, function(market, mode) {
+        router.go(format(
+            'trade/%s/%s/%s',
+            market,
+            mode,
+            $.cookie('tradeType') == 'sell' ? 'sell' : 'buy'),
+        true)
     })
     .add(/^trade\/([A-Z]{6})\/(instant|advanced)\/(buy|sell)$/,
         function(market, mode, type)
     {
         if (!authorize.user(2)) return
 
-        mode && $.cookie('tradeMode', mode, { expires: 10 * 356 * 7 })
-        market && $.cookie('tradeMarket', market, { expires: 10 * 356 * 7 })
-        type && $.cookie('tradeType', type, { expires: 10 * 356 * 7 })
+        $.cookie('tradeMode', mode, { expires: 10 * 356 * 7 })
+        $.cookie('tradeMarket', market, { expires: 10 * 356 * 7 })
+        $.cookie('tradeType', type, { expires: 10 * 356 * 7 })
 
         mode = mode == 'instant' ? 'market' : 'limit'
         type = type == 'buy' ? 'bid' : 'ask'
+
         master(require('./market')(market, mode, type), 'trade')
     })
-    .add(/^trade\/([A-Z]{6})\/stats$/,
-        function(market)
-    {
-        master(require('./stats')(market), 'trade')
+    .add(/^trade\/orders$/, function() {
+        if (!authorize.user()) return
+        master(require('./orders')(), 'trade')
     })
 }
