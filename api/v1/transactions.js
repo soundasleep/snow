d1c0d8ddf2c7a54cd5bbbbfd298b2677f709a36a
Andreas Brekken <a@abrkn.com>
2013-09-24 21:38:46 +0100

    Add CSV download of user transactions. ref justcoin/justcoin#147

--- api/v1/transactions.js
+++ api/v1/transactions.js
@@ -3,32 +3,71 @@ var transactions = require('../transactions')
 
 module.exports = exports = function(app) {
     app.post('/v1/transactions', app.security.demand.any, exports.index)
+    app.get('/v1/transactions/csv', app.security.demand.any, exports.csv)
+    exports.app = app
 }
 
-exports.index = function(req, res, next) {
+exports.query = function(userId, skip, cb) {
     var query = {
-        userId: req.user.id,
+        userId: userId,
         sort: {
             timestamp: 'desc'
         },
         limit: 20
     }
 
-    if (req.body.skip) {
-        query.skip = +req.body.skip
+    if (skip) {
+        query.skip = skip
     }
 
-    transactions.query(req.app, query, function(err, sr) {
-        if (err) return next(err)
+    transactions.query(exports.app, query, function(err, sr) {
+        if (err) return cb(err)
 
-        res.send({
+        cb(null, {
             count: sr.count,
             limit: sr.limit,
             transactions: sr.transactions.map(function(tran) {
                 return _.extend({
-                    amount: tran.creditUserId == req.user.id ? tran.amount : '-' + tran.amount
+                    amount: tran.creditUserId == userId ? tran.amount : '-' + tran.amount
                 }, _.pick(tran, 'id', 'type', 'timestamp', 'date', 'currency'))
             })
         })
+    })
+}
+
+exports.index = function(req, res, next) {
+    var skip = req.body.skip ? +req.body.skip : null
+    exports.query(req.user.id, skip, function(err, qres) {
+        if (err) return next(err)
+        res.send(qres)
+    })
+}
+
+exports.csv = function(req, res, next) {
+    var skip = req.query.skip ? +req.query.skip : null
+    exports.query(req.user.id, skip, function(err, qres) {
+        if (err) return next(err)
+
+        var csv = [
+            ['id', 'type', 'timestamp', 'date', 'currency', 'amount']
+        ]
+
+        csv = csv.concat(qres.transactions.map(function(row) {
+            return [
+                row.id,
+                row.type,
+                row.timestamp,
+                row.date,
+                row.currency,
+                exports.app.cache.formatCurrency(row.amount, row.currency)
+            ]
+        }))
+
+        res.header('Content-Type', 'text/csv; name="transactions.csv"')
+        res.header('Content-Disposition', 'attachment; filename="transactions.csv"')
+
+        res.send(csv.map(function(row) {
+            return row.join(',')
+        }).join('\n'))
     })
 }
