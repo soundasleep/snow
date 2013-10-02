c43f4632c05568bb019bbc605446d3b63e14ef90
Andreas Brekken <a@abrkn.com>
2013-10-02 13:19:24 +0200

    Fix validation. Close justcoin/justcoin#165

--- frontend/helpers/validation.js
+++ frontend/helpers/validation.js
@@ -1,6 +1,62 @@
 var _ = require('lodash')
 
-module.exports = exports = function() {
-    var validator = $.when.apply($, _.toArray(arguments))
-    return validator
+/**
+ * Returns a deferred that validates multiple fields in
+ * the form { field1: deferred1, field2: deferred2 }
+ */
+exports.validateFields = function(fields) {
+    return $.when.apply($, _.values(fields))
+    .then(function() {
+        return _.reduce(arguments, function(results, result, index) {
+            results[_.keys(fields)[index]] = result
+            return results
+        }, {})
+    })
+}
+
+exports.validateGroup = function($group, fn, submitting) {
+    $group.removeClasses(/^(is|has)-/)
+    var val = $group.field().val()
+    , deferred = $.Deferred()
+    .fail(function(code) {
+        $group.addClass('has-error ' + code)
+    })
+
+    if (!val.length && submitting !== true) return deferred.resolve(null)
+
+    return fn(deferred, val)
+}
+
+exports.validateRegex = function($group, expr, submitting) {
+    return exports.validateGroup($group, function(deferred, val) {
+        if (val.match(expr)) deferred.resolve(val)
+        else deferred.reject('is-invalid')
+        return deferred
+    }, submitting)
+}
+
+exports.fromRegex = function($group, expr) {
+    return _.bind(exports.validateRegex, exports, $group, expr)
+}
+
+exports.monitorField = function($field, validator) {
+    $field.on('change', function() {
+        validator()
+    })
+
+    $field.on('keyup', function(e) {
+        if (~[13, 7, 27].indexOf(e.which)) return
+        validator()
+    })
+}
+
+exports.fromFields = function(fields) {
+    return function(submitting) {
+        var validators = _.reduce(_.keys(fields), function(p, name) {
+            p[name] = fields[name](submitting)
+            return p
+        }, {})
+
+        return exports.validateFields(validators)
+    }
 }
