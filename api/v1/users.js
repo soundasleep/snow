fe317b7543404f1c70cbe7cd5da469f9466dc437
Andreas Brekken <a@abrkn.com>
2013-10-14 08:59:31 +0200

    Re-implement change password.

--- api/v1/users.js
+++ api/v1/users.js
@@ -9,6 +9,7 @@ module.exports = exports = function(app) {
     app.post('/v1/users/verify/call', app.security.demand.primary(1), exports.startPhoneVerify)
     app.post('/v1/users/verify', app.security.demand.primary(1), exports.verifyPhone)
     app.patch('/v1/users/current', app.security.demand.primary, exports.patch)
+    app.post('/v1/changePassword', app.security.demand.otp(app.security.demand.primary, true), exports.changePassword)
 }
 
 exports.patch = function(req, res, next) {
@@ -268,5 +269,23 @@ exports.startPhoneVerify = function(req, res, next) {
             if (err) return next(err)
             res.send(204)
         })
+    })
+}
+
+exports.changePassword = function(req, res, next) {
+    if (!req.app.validate(req.body, 'v1/users_changepassword', res)) return
+
+    req.app.conn.write.query({
+        text: [
+            'UPDATE api_key',
+            'SET api_key_id = $2',
+            'WHERE user_id = $1 AND "primary" = TRUE'
+        ].join('\n'),
+        values: [req.user.id, req.body.key]
+    }, function(err) {
+        if (err) return next(err)
+        req.app.activity(req.user.id, 'ChangePassword', {})
+        res.send(204)
+        req.app.security.invalidate(req.user.id)
     })
 }
