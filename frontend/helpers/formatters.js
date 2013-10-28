2f358217c2534ef9b190a5fa10826dac6cb8d223
Andreas Brekken <a@abrkn.com>
2013-10-28 17:19:34 +0100

    Remove console.log's from frontend.

--- frontend/helpers/formatters.js
+++ frontend/helpers/formatters.js
@@ -1,7 +1,4 @@
-var format = require('util').format
-
 exports.bankAccount = function(x) {
-    console.log(x.iban || x.accountNumber)
     return (x.displayName !== null ? x.displayName + ' (' : '') +
         (x.iban || x.accountNumber) +
         (x.displayName !== null ? ')' : '')
