c6d813cf28dfeee643cec4795a1627dc41025af2
Frederic Heem <frederic.heem@gmail.com>
2013-11-01 22:53:52 +0100

    add snow client method: whoiam, balances and showall, add mocha test

--- client/index.js
+++ client/index.js
@@ -1,11 +1,16 @@
 var debug = require('debug')('snow')
 , assert = require('assert')
 , request = require('request')
-, Snow = module.exports = function(key, ep) {
+, num = require('num')
+, async = require('async')
+, Table = require('cli-table')
+, Snow = module.exports = function(key, ep, email, keyPrimary) {
     this.url = ep
     debug('using endpoint %s', ep)
 
     this.key = key
+    this.keyPrimary = keyPrimary
+    this.email = email
 }
 
 function bodyToError(body) {
@@ -68,6 +73,32 @@ Snow.prototype.cancel = function(id, cb) {
     })
 }
 
+Snow.prototype.cancelAll = function(cb) {
+	var me = this
+	var orderCancelled = 0;
+	this.orders( function(err, orders) {
+		if (err) throw err
+		if (orders.length == 0){
+			cb(null, 0)
+			return;
+		}
+		async.forEach(
+				orders,
+				function(order, callback) {
+					me.cancel(order.id, function(err) {
+						if (err) throw err
+						debug('Order #%s cancelled', order.id)
+						orderCancelled++;
+						callback()
+					})
+				}, 
+				function(err) {
+					cb(err, orderCancelled)
+				}
+		);
+	})
+}
+
 Snow.prototype.order = function(order, cb) {
     request(this.url + 'orders', {
         json: order,
@@ -80,4 +111,119 @@ Snow.prototype.order = function(order, cb) {
         if (res.statusCode !== 201) return cb(bodyToError(body))
         cb(null, body.id)
     })
+}
+
+Snow.prototype.whoami = function(cb) {
+    request(this.url + 'whoami', {
+        json: true,
+        qs: {
+            key: this.key
+        }
+    }, function(err, res, body) {
+        if (err) return cb(err)
+        if (res.statusCode !== 200) return cb(bodyToError(body))
+        cb(null, body)
+    })
+}
+
+Snow.prototype.balances = function(cb) {
+    request(this.url + 'balances', {
+        json: true,
+        qs: {
+            key: this.key
+        }
+    }, function(err, res, body) {
+        if (err) return cb(err)
+        if (res.statusCode !== 200) return cb(bodyToError(body))
+        cb(null, body)
+    })
+}
+
+Snow.prototype.securitySession = function(cb) {
+	
+    request(this.url + 'security/session', {
+        json: {"email": this.email},
+        method: 'POST'
+    }, function(err, res, body) {
+        if (err) return cb(err)
+        if (res.statusCode !== 201) return cb(bodyToError(body))
+        this.sessionKey = body.id;
+        cb(null, body.id)
+    })
+}
+
+Snow.prototype.createTableUser = function(user){
+	var table = new Table({
+        head: ['Id', 'Email', 'Security Level', 'twoFactor', 'First name', 'Last name', 'Phone', 'Email verified' ],
+        colWidths: [4, 24, 14, 12, 16, 16, 16, 16]
+    })
+    table.push([
+                user.id,
+                user.email || '',
+                user.securityLevel || '',
+                user.twoFactor ? 'Yes' : 'No',
+                user.firstName || '',
+                user.lastName || '',
+                user.phone || '',
+                user.emailVerified ? 'Yes' : 'No'
+            ]);
+    return table
+}
+
+Snow.prototype.createTableBalances = function(balances){
+	var table = new Table({
+        head: ['Currency', 'Balance', 'Hold', 'Available'],
+        colWidths: [12, 12, 12, 12]
+    })
+
+	balances.forEach(function(balance) {
+        table.push([
+            balance.currency,
+            balance.balance,
+            balance.hold,
+            balance.available
+        ])
+    })
+    
+    return table
+}
+
+Snow.prototype.createTableMarkets = function(markets){
+	var table = new Table({
+        head: ['Market', 'Bid', 'Ask', 'Last', 'High', 'Low', 'Volume'],
+        colWidths: [8, 12, 12, 12, 12, 12, 12]
+    })
+
+    markets.forEach(function(market) {
+        table.push([
+            market.id,
+            market.bid || '',
+            market.ask || '',
+            market.last || '',
+            market.high || '',
+            market.low || '',
+            market.volume || '0'
+        ])
+    })
+    return table
+}
+
+Snow.prototype.createTableOrders = function(orders){
+	var table = new Table({
+        head: ['#', 'Market', 'Type', 'Volume', 'Price', 'Total'],
+        colWidths: [8, 10, 6, 20, 20, 20]
+    })
+
+    orders.forEach(function(order) {
+        var pair = [order.market.substr(0, 3), order.market.substr(3)]
+        table.push([
+            order.id,
+            order.market,
+            order.type.toUpperCase(),
+            order.remaining + ' ' + pair[0],
+            order.price + ' ' + pair[1],
+            num(order.remaining).mul(order.price) + ' ' + pair[1]
+        ])
+    })
+    return table
 }
---
+++ client/test/testSnow.js
@@ -0,0 +1,141 @@
+﻿/*global describe, it, before, after*/
+var assert = require('assert');
+var request = require('supertest');
+var async = require('async');
+var config = require('./configTest.js')();
+var num = require('num');
+var debug = require('debug')('snowtest')
+
+describe('SnowClient', function () {
+    "use strict";
+    
+    var alice_config = config.alice;
+    var bob_config = config.bob;
+    var client = new (require('../../client/index'))(alice_config.apikey, config.url, alice_config.email, alice_config.key);
+    var client_bob = new (require('../../client/index'))(bob_config.apikey, config.url, bob_config.email);
+    var sessionKey;    
+    var displayDepth = function(depth){
+    	if(!depth){
+    		console.error("Invalid depth")
+    		return
+    	}
+    	console.log(depth)
+    }
+    
+    describe('Session', function () {
+        it('SessionCreate', function (done) {
+        	client.securitySession(function(err, key) {
+        		if (err) throw err
+        		sessionKey = key
+        		debug("securitySession key %s", key)
+        		done()
+        	});
+        });
+    });
+    
+    describe('Markets', function () {
+        it('market', function (done) {
+        	client.markets(function(err, markets) {
+                if (err) throw err
+                console.log(client.createTableMarkets(markets).toString())
+                done();
+            })
+        });
+    });
+    
+    describe('Whoami', function () {
+        it('WhoamiAlice', function (done) {
+        	client.whoami(function(err, user) {
+        		if (err) throw err
+        		console.log(client.createTableUser(user).toString())
+        		done()
+        	});
+        });
+        it('WhoamiBob', function (done) {
+        	client_bob.whoami(function(err, user) {
+        		if (err) throw err
+        		console.log(client.createTableUser(user).toString())
+        		done()
+        	});
+        });        
+    });
+    
+    describe('Depth', function () {
+        it('Depth', function (done) {
+        	client.depth(config.market, function(err, depth) {
+                if (err) throw err
+                displayDepth(depth)
+                done()
+            })
+        });
+    });
+
+    describe('Balances', function () {
+        it('BalancesAlice', function (done) {
+        	client.balances(function(err, balances) {
+        		if (err) throw err
+        		console.log(client.createTableBalances(balances).toString())
+        		done()
+        	});
+        });
+        it('BalancesBob', function (done) {
+        	client_bob.balances(function(err, balances) {
+        		if (err) throw err
+        		console.log(client.createTableBalances(balances).toString())
+        		done()
+        	});
+        });
+    });
+
+    describe('Orders', function () {
+        it('aliceOrders', function (done) {
+        	client.orders( function(err, orders) {
+        		if (err) throw err
+        		console.log(client.createTableOrders(orders).toString())
+                done()
+            })
+        });
+        
+        it('aliceBid', function (done) {
+        	client.order({
+                market: config.market,
+                type: "bid",
+                price: config.bid_price,
+                amount: config.volume
+            }, function(err, id) {
+                if (err) throw err
+                debug('Order bid #%s placed', id)
+                done()
+            })
+        });
+        it('bobAsk', function (done) {
+        	client_bob.order({
+                market: config.market,
+                type: "ask",
+                price: config.ask_price,
+                amount: config.volume
+            }, function(err, id) {
+                if (err) throw err
+                debug('Order ask #%s placed', id)
+                done()
+            })
+        });
+        it('bobOrders', function (done) {
+        	client_bob.orders( function(err, orders) {
+        		if (err) throw err
+        		console.log(client.createTableOrders(orders).toString())
+                done()
+            })
+        });    
+    });
+    
+    describe('CancelOrders', function () {
+    	it('CancelOrdersAlice', function (done) {
+    		client.cancelAll(function(err, orderCancelled) {
+    			if (err) throw err
+    			debug("#orderCancelled: %d", orderCancelled)
+    			done()
+    		});
+    	});
+    });
+});
